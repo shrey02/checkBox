c5b58e5aeef3cfe7d81edacb1c1e9113bd1fd7d1
Shrey <sinhashrey2206@gmail.com>
2023-10-25 22:05:37 +0530

    implemented checkbox component

---
+++ checkbox/src/assets/data.js
@@ -0,0 +1,46 @@
+const obj = [
+    {
+      id: 1,
+      id: "class",
+      value: false,
+      children: [
+        {
+          id: 2,
+          id: "sec A",
+          value: false,
+          children: [
+            {
+              id: 3,
+              id: "shrey",
+              value: false
+            },
+            {
+              id: 4,
+              id: "saqib",
+              value: false
+            }
+          ]
+        },
+        {
+          id: 5,
+          id: "sec B",
+          value: false,
+          children: [
+            {
+              id: 6,
+              id: "shrey1",
+              value: false
+            },
+            {
+              id: 7,
+              id: "saqib2",
+              value: false
+            }
+          ]
+        }
+      ]
+    }
+  ];
+  
+  export default obj;
+  
